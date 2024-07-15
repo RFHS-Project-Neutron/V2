e0be09321505d7f6ef8dda2ecf066140da54631d
Christian Loga <148154322+PlayFlixo@users.noreply.github.com>
2024-07-15 14:39:23 -0500

    Create analytics.js

---
+++ js/analytics.js
@@ -0,0 +1,13 @@
+// Microsoft Clarity, placed 7/15/2024
+(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "n7w6y6wzt8");
+
+// Google Analytics, placed 7/15/2024
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-LVCEWNB2TH');
