8993dc548955f192f00c8aaa17c75a36fcbee88d
Christian Loga <148154322+PlayFlixo@users.noreply.github.com>
2024-07-18 14:46:53 -0500

    Update embeds.js

--- js/embeds.js
+++ js/embeds.js
@@ -1,5 +1,6 @@
 const embeds = {
-    '2048': '/gs/2048',
+    'subway-surfers': '/gs/subway-surfers',
     'gswitch': '/gs/gswitch',
+    '2048': 'https://testing-v2.pages.dev/gs/2048/',
     'default': '/gs/default'
 };
