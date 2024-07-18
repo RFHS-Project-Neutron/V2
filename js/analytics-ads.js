40c46e7dfdaf94b0ce3a1710534682a654d75717
Christian Loga <148154322+PlayFlixo@users.noreply.github.com>
2024-07-18 11:36:41 -0500

    Create analytics-ads.js

---
+++ js/analytics-ads.js
@@ -0,0 +1,23 @@
+// analytics-ads.js
+
+// Google Analytics (gtag.js)
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-LVCEWNB2TH');
+
+// Microsoft Clarity
+(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "n7w6y6wzt8");
+
+// Monetization Tag
+document.querySelector('meta[name="monetag"]').setAttribute('content', '26b77ec34fbc49291359b7365c16fcb7');
+
+// thubanoa.com async script
+var thubanoaScript = document.createElement('script');
+thubanoaScript.async = true;
+thubanoaScript.src = "//thubanoa.com/1?z=7372549";
+document.head.appendChild(thubanoaScript);
