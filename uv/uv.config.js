130c87ae42dc21c6ef9a1faa726e92e606b394f3
Christian Loga <148154322+PlayFlixo@users.noreply.github.com>
2024-08-28 23:42:51 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,12 +1,12 @@
-/*global Ultraviolet*/
+// This file overwrites the stock UV config.js
+
 self.__uv$config = {
-	prefix: '/uv/service/',
-	bare: 'https://bare-server-liard.vercel.app/', // https://infiniteknowledge.online/bare/, https://scholarlyessay.info/bare/
-	encodeUrl: Ultraviolet.codec.xor.encode,
-	decodeUrl: Ultraviolet.codec.xor.decode,
-	handler: '/uv/uv.handler.js',
-	client: '/uv/uv.client.js',
-	bundle: '/uv/uv.bundle.js',
-	config: '/uv/uv.config.js',
-	sw: '/uv/uv.sw.js'
+  prefix: "/uv/service/",
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: "/uv/uv.handler.js",
+  client: "/uv/uv.client.js",
+  bundle: "/uv/uv.bundle.js",
+  config: "/uv/uv.config.js",
+  sw: "/uv/uv.sw.js",
 };
